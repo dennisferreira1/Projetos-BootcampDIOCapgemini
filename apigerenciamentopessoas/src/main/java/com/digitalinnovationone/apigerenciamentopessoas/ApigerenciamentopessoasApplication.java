4c87ab70eac4044d9d47ebc08bc76c65e1e53589
dennisferreira1 <dennisferreira1@hotmail.com>
2021-09-05 22:30:39 -0300

    criacao do projeto

---
+++ apigerenciamentopessoas/src/main/java/com/digitalinnovationone/apigerenciamentopessoas/ApigerenciamentopessoasApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.apigerenciamentopessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigerenciamentopessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigerenciamentopessoasApplication.class, args);
+	}
+
+}
---
+++ apigerenciamentopessoas/src/test/java/com/digitalinnovationone/apigerenciamentopessoas/ApigerenciamentopessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.apigerenciamentopessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigerenciamentopessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
