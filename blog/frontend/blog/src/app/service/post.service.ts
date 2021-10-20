5a0ca4515f9b7fde7e1f82f7b9381cf3bd1117c1
dennisferreira1 <dennisferreira1@hotmail.com>
2021-10-19 21:24:41 -0300

    mudando url do backend

--- blog/frontend/blog/src/app/service/post.service.ts
+++ blog/frontend/blog/src/app/service/post.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 import { Post } from '../model/post';
 
-const url = "http://localhost:3000/posts"
+const url = "https://blog-desoftwares-server.herokuapp.com/posts"
 
 @Injectable({
   providedIn: 'root'
@@ -26,17 +26,6 @@ export class PostService {
 
     return this.http.get<Post[]>(url, {params: params})
   }
-  
-  /* getPostsPorNome(nome: string): Observable<Post[]> {
-    if(nome) {
-      let params = new HttpParams();
-      // configurando parâmetro para buscar por nome, ignorando maiúsculas e minúsculas
-      params = params.set('nome_like', nome);
-      return this.http.get<Post[]>(url, {params: params})
-    } else {
-      return this.getPosts();
-    }
-  } */
 
   addPost(post: Post): Observable<any> {
     return this.http.post(url, post);
